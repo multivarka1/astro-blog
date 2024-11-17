bca7bbca76f2703de67a830ccf8e9dcb79577cb3
multivarka1 <117528956+multivarka1@users.noreply.github.com>
2024-11-17 22:33:56 +0300

    Add files via upload

---
+++ consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Сайт о домашних животных';
+export const SITE_DESCRIPTION = 'Добро пожаловать!';
